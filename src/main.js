31aae386171ae4a1c8b60b1d49684ef8a7b87dfd
songzhongyu <laotian2019szy@163.com>
2020-03-21 21:23:39 +0800

    安装axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui' // 引入包
 import 'element-ui/lib/theme-chalk/index.css' // 引入element样式
 import './style/index.less' // 引入初始化样式
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 赋值默认地址API
+Vue.prototype.$axios = axios // 赋值全局对象
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册
 new Vue({
